fe4593a776a8263120b1483a2ff76840e457502a
singlanitin661 <singlanitin661@gmail.com>
2022-06-10 16:28:56 +0530

    Enter

---
+++ src/NavBar.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="nav-container">
+          <span className="nav-logo">
+            IMAGES
+            <i className="fas fa-code"></i>
+          </span>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <span
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </span>
+            </li>
+            <li className="nav-item">
+              <span
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </span>
+            </li>
+            <li className="nav-item">
+              <span
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Event
+              </span>
+            </li>
+            <li className="nav-item">
+              <span
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </span>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;

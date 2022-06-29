4973717e04f6a34e203d04358f512f685ecc8757
singlanitin661 <singlanitin661@gmail.com>
2022-06-29 10:08:17 +0530

    Enter

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,84 +1,85 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./Contact.css";
-import insta from "../Images/insta.png";
-import facebook from "../Images/facebook.png";
-import linkedin from "../Images/linkedin.png";
-import youtube from "../Images/youtube.png";
 
-function Contact() {
+export default function Contact() {
   return (
-    <div>
-      <section className="contact_us">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-10 offset-md-1">
-              <div className="contact_inner">
-                <div className="row">
-                  <div className="col-md-10">
-                    <div className="contact_form_inner">
-                      <div className="contact_field">
-                        <h1>Contact Us</h1>
-                        <p>
-                          Feel free to contact us for your queries or Just say
-                          Hello😃
-                        </p>
-                        <input
-                          type="text"
-                          className="form-control form-group"
-                          placeholder="Name"
-                        />
-                        <input
-                          type="text"
-                          className="form-control form-group"
-                          placeholder="Email"
-                        />
-                        <textarea
-                          className="form-control form-group"
-                          placeholder="Message"
-                        ></textarea>
-                        <button className="contact_form_submit">Send</button>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="right_conatct_social_icon d-flex align-items-end"></div>
-                  </div>
-                </div>
-                <div className="contact_info_sec">
-                  <h4>Contact Info</h4>
-                  <div className="d-flex info_single align-items-left">
-                    <span>+91 8009 054294</span>
-                  </div>
-                  <div className="d-flex info_single align-items-left">
-                    <span>images.desk@gmail.com</span>
-                  </div>
-                  <div className="info_single_name ">
-                    <span>Registered under chandigarh administration.</span>
-                    <div style={{ padding: "7px", display: "flex" }}>
-                      <a href="https://www.facebook.com/imagesonnet/">
-                        <img src={facebook} />
-                      </a>
-                      <a href="https://instagram.com/images_chandigarh?igshid=16blcl5andwr3">
-                        <img src={insta} />
-                      </a>
-                      <a href="https://www.linkedin.com/company/images.chandigarh/">
-                        <img src={linkedin} />
-                      </a>
-                      <a>
-                        <img src={youtube} />
-                      </a>
-                    </div>
-                  </div>
-                </div>
+    <div className="root">
+      <section id="contact">
+        <div className="contact-box">
+          <div className="contact-links">
+            <h2 className="contact_h2">CONTACT</h2>
+            <div className="links">
+              <div className="link">
+                <a>
+                  <img
+                    className="contact_img"
+                    src="https://i.postimg.cc/m2mg2Hjm/linkedin.png"
+                    alt="linkedin"
+                  />
+                </a>
+              </div>
+              <div className="link">
+                <a>
+                  <img
+                    className="contact_img"
+                    src="https://i.postimg.cc/YCV2QBJg/github.png"
+                    alt="github"
+                  />
+                </a>
+              </div>
+              <div className="link">
+                <a>
+                  <img
+                    className="contact_img"
+                    src="https://i.postimg.cc/W4Znvrry/codepen.png"
+                    alt="codepen"
+                  />
+                </a>
+              </div>
+              <div className="link">
+                <a>
+                  <img
+                    className="contact_img"
+                    src="https://i.postimg.cc/NjLfyjPB/email.png"
+                    alt="email"
+                  />
+                </a>
               </div>
             </div>
           </div>
+          <div className="contact-form-wrapper">
+            <form>
+              <div className="form-item">
+                <input
+                  className="contact_input"
+                  type="text"
+                  name="sender"
+                  required
+                />
+                <label className="contact_label">Name:</label>
+              </div>
+              <div className="form-item">
+                <input
+                  className="contact_input"
+                  type="text"
+                  name="email"
+                  required
+                />
+                <label className="contact_label">Email:</label>
+              </div>
+              <div className="form-item">
+                <textarea
+                  className="contact_textarea"
+                  name="message"
+                  required
+                ></textarea>
+                <label className="contact_label">Message:</label>
+              </div>
+              <button className="submit-btn">Send</button>
+            </form>
+          </div>
         </div>
       </section>
-
-      
     </div>
   );
 }
-
-export default Contact;
--- src/Components/Navbar/Carousel.js
+++ src/Components/Navbar/Carousel.js
@@ -1,21 +1,40 @@
 import React from "react";
-import './Carousel.css'
-import myImage1 from '../Images/1.jpg'
-import myImage2 from '../Images/2.jpg'
-import myImage3 from '../Images/3.jpg'
+import "./Carousel.css";
+import myImage1 from "../Images/1.jpg";
+import myImage2 from "../Images/2.jpg";
+import myImage3 from "../Images/3.jpg";
 function Carousel() {
   return (
     <div className="root">
-      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+      <div
+        id="carouselExampleSlidesOnly"
+        className="carousel slide"
+        data-bs-ride="carousel "
+      >
         <div className="carousel-inner  size">
           <div className="carousel-item active">
-            <img src={myImage1} className="d-block h-10 w-100 img" style={{height:'500px'}} alt="slide 1" />
+            <img
+              src={myImage1}
+              className="d-block h-10 w-100 img"
+              style={{ height: "500px" }}
+              alt="slide 1"
+            />
           </div>
           <div className="carousel-item  size">
-            <img src={myImage2} className="d-block h-10 w-100 img" style={{height:'500px'}} alt="slide 2" />
+            <img
+              src={myImage2}
+              className="d-block h-10 w-100 img"
+              style={{ height: "500px" }}
+              alt="slide 2"
+            />
           </div>
           <div className="carousel-item  size">
-            <img src={myImage3} className="d-block h-10 w-100 img" style={{height:'500px'}} alt="slide 3" />
+            <img
+              src={myImage3}
+              className="d-block h-10 w-100 img"
+              style={{ height: "500px" }}
+              alt="slide 3"
+            />
           </div>
         </div>
       </div>
